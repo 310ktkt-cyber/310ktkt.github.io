e6026b1649caad9aa06096d4eaac22fc9529ad79
310ktkt-cyber <310ktkt@gmail.com>
2026-09-01 20:01:39 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/310ktkt.github.io/',
   plugins: [
     react(),
     VitePWA({
